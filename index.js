bdcfa95033c96e9400c70fbb16b8488f1141414d
Dhanny <dannysark16@gmail.com>
2020-02-23 04:44:56 +0000

    added persistency to the application

--- index.js
+++ index.js
@@ -4,11 +4,42 @@
 
 // Dependencies
 var http = require('http');
+var https = require('https');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./lib/config');
+var fs = require('fs');
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
- // Configure the server to respond to all requests with a string
-var server = http.createServer(function(req,res) {
+// instanciate the http server
+var httpServer = http.createServer(function(req,res) {
+  unifiedServer(req, res);
+});
 
+// Start the server
+httpServer.listen(config.httpPort, function() {
+  console.log('The server is up and running ' + config.httpPort + ' now');
+});
+
+// instantiate the https server
+var httpsServerOptions = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = https.createServer(httpsServerOptions, function(req,res) {
+  unifiedServer(req, res);
+});
+
+// start the https server
+httpsServer.listen(config.httpsPort, function() {
+  console.log('The server is up and running ' + config.httpsPort + ' now');
+});
+
+// all the server logic for both the http and the https server
+var unifiedServer = function(req, res) {
+  
   // Parse the url
   var parsedUrl = url.parse(req.url, true);
 
@@ -22,14 +53,54 @@ var server = http.createServer(function(req,res) {
   // get the HTTP method
   var method = req.method.toLowerCase();
 
-  // Send the response
-  res.end('Hello World!\n');
+  // get the headers as an object
+  var headers = req.headers;
 
-  // Log the request/response
-  console.log('Request received on path: '+trimmedPath + ' with the method: ' +method + ' with query string parameters: ' ,queryStringObject);
-});
+  // get the payload if there is any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', function() {
+    buffer += decoder.end();
 
-// Start the server
-server.listen(3000, function() {
-  console.log('The server is up and running now');
-});
+    // choose the handler the request should go to, if not found route to not found
+    var choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notfound;
+
+    // construct the data object to send to the handler
+    var data = {
+      'trimedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': helpers.parseJsonToObject(buffer)
+    }
+
+    // route the request to the handler specified in the router
+    choosenHandler(data, function(statusCode, payload) {
+      // use the status code called back by the handler or defalut to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+      // use the payload code called back by the handler or defalut
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // convert the payload to a string
+      var payloadString = JSON.stringify(payload);
+
+      // Return the reponse
+      res.setHeader('content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log the request/response
+      console.log('Returning this response: ', statusCode, payload);
+    });
+  });
+};
+
+//define a request router
+var router = {
+  'ping': handlers.ping,
+  'users': handlers.users
+};
